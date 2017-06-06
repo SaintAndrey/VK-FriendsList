5c8549f420673783ead0bc394f9afc8212199995
Andrey <andrersv96@gmail.com>
2017-06-06 10:56:03 +0300

    Initial Commit

---
+++ VK FriendsList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VK FriendsList
+//
+//  Created by Andrey on 06/06/2017.
+//  Copyright © 2017 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VK FriendsList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VK FriendsList
+//
+//  Created by Andrey on 06/06/2017.
+//  Copyright © 2017 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
